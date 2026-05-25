9fccdf87029bad113b0a0c2b3179903deb5a421a
Brendon Rell <brendonharryrell@gmail.com>
2026-05-25 07:31:56 -0400

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -280,6 +280,7 @@ const LOADER_HTML = `<style>
     display: flex;
     align-items: center;
     justify-content: center;
+    background: var(--bg-color);
     backdrop-filter: blur(22px);
     -webkit-backdrop-filter: blur(22px);
   }
