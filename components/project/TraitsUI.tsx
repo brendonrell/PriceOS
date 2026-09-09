26555aefa961072660b0e8351938a3f6fbc61734
Brendon Rell <brendonharryrell@gmail.com>
2026-09-09 06:42:20 -0400

    Add files via upload

--- components/project/TraitsUI.tsx
+++ components/project/TraitsUI.tsx
@@ -1175,7 +1175,7 @@ export default function TraitsUI({
                            the chip row below spells out the rest). */
                         glyph={groupLayers.length ? GROUP_GLYPH[groupLayers[0]!] : GROUP_GLYPH[effGroup]}
                         on={groupLayers.length > 0 || effGroup !== 'none'}
-                        onTapAnchor={(a) => setLayersAnchor({ ...a, startAt: 1 })}
+                        onTapAnchor={(a) => setLayersAnchor((prev) => (prev ? null : { ...a, startAt: 1 }))}
                         onHold={(a) => setLayersAnchor({ ...a, startAt: null })}
                     />
                     {layersAnchor && (
