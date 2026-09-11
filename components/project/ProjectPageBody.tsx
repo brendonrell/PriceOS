348ea572da3ee6a3b3ac941288daf7be41dcffc6
Brendon Rell <brendonharryrell@gmail.com>
2026-09-11 08:52:12 -0400

    Add files via upload

--- components/project/ProjectPageBody.tsx
+++ components/project/ProjectPageBody.tsx
@@ -429,11 +429,11 @@ function ProjectPageBodyInner({ uploadedAt = null, projectNo = null }: { uploade
                             <span className="cbr-label">Collected by&nbsp;</span>
                             <a key={collectors[0]} className="profile-link feat-name" href={`/${handle(collectors[0])}`}>@{handle(collectors[0])}</a>
                             {collectors[1] ? (
-                                <>,&nbsp;<a key={collectors[1]} className="profile-link feat-name" href={`/${handle(collectors[1])}`}>@{handle(collectors[1])}</a> </>
-                            ) : ' '}
+                                <>,&nbsp;<a key={collectors[1]} className="profile-link feat-name" href={`/${handle(collectors[1])}`}>@{handle(collectors[1])}</a></>
+                            ) : null}
                             {others > 0 && (
                                 <span className="cbr-others" onClick={() => open('collectors', undefined, project.slug)}>
-                                    &amp;&nbsp;{others}&nbsp;more&nbsp;you&nbsp;follow
+                                    &nbsp;&amp;&nbsp;{others}&nbsp;more&nbsp;you&nbsp;follow
                                 </span>
                             )}
                         </div>
